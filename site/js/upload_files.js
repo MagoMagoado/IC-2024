aaca38631b3d75af7b0bcc45f0b49c99b2e6fc27
Isabella <isabella-arj@hotmail.com>
2024-03-13 14:52:41 -0300

    carregando files

--- site/js/upload_files.js
+++ site/js/upload_files.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     const $ = document.querySelector.bind(document);
+	const loadFiles = `
+		<div class="carregandoFiles">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4335 4335">
+						<path fill="#4db6ac" d="M3346 1077c41,0 75,34 75,75 0,41 -34,75 -75,75 -41,0 -75,-34 -75,-75 0,-41 34,-75 75,-75zm-1198 -824c193,0 349,156 349,349 0,193 -156,349 -349,349 -193,0 -349,-156 -349,-349 0,-193 156,-349 349,-349zm-1116 546c151,0 274,123 274,274 0,151 -123,274 -274,274 -151,0 -274,-123 -274,-274 0,-151 123,-274 274,-274zm-500 1189c134,0 243,109 243,243 0,134 -109,243 -243,243 -134,0 -243,-109 -243,-243 0,-134 109,-243 243,-243zm500 1223c121,0 218,98 218,218 0,121 -98,218 -218,218 -121,0 -218,-98 -218,-218 0,-121 98,-218 218,-218zm1116 434c110,0 200,89 200,200 0,110 -89,200 -200,200 -110,0 -200,-89 -200,-200 0,-110 89,-200 200,-200zm1145 -434c81,0 147,66 147,147 0,81 -66,147 -147,147 -81,0 -147,-66 -147,-147 0,-81 66,-147 147,-147zm459 -1098c65,0 119,53 119,119 0,65 -53,119 -119,119 -65,0 -119,-53 -119,-119 0,-65 53,-119 119,-119z"
+						/>
+					</svg>
+		</div>
+		`;
 
     let App = {};
     App.init = (function() {
         //Init
         function handleFileSelect(evt) {
             const files = evt.target.files; // FileList object
+
 			
-			$(".lines-update").innerHTML = ``;
+			$(".lines-update").innerHTML = loadFiles;
 
 			$("#drop").classList.add("hidden");
 			$("#files-active").classList.remove("hidden");
 
+			
 			let linesFiles = `${Object.keys(files)
 				.map(file =>
 					`<div class="linha">
@@ -22,11 +32,17 @@ document.addEventListener("DOMContentLoaded", function() {
 					</div>
 					`)
 				.join("")}`;
+
 			
-			setTimeout(() => {
-				$(".lines-update").innerHTML = `${linesFiles}`;
-			}, 300);
+			Object.keys(files).forEach(file => {
+				let load = 2000 + (file * 2000); // fake load
+				setTimeout(() => {
+					$(".lines-update").innerHTML = `${linesFiles}`;
+				}, load);
+			});
         }
+
+		
 
 		//trigger
         $("#triggerFile").addEventListener("click", evt => {
@@ -34,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
             $("input[type=file]").click();
         });
 		// drop events
-				$("#drop").ondragleave = evt => {
+		$("#drop").ondragleave = evt => {
 			$("#drop").classList.remove("active");
 			evt.preventDefault();
 		};
@@ -50,6 +66,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		};
 		//cancel
 		$("#cancel-upload-files").addEventListener("click", evt => {
+			$(".lines-update").innerHTML = ``;
             evt.preventDefault();
 			$("#files-active").classList.add("hidden");
 			$("#drop").classList.remove("hidden");
